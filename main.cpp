736cc98c588247178bb838f9d83a8f19c0bd104f
vinso <44764023+Vring@users.noreply.github.com>
2022-04-12 12:12:43 +1000

    Миникарта опциональна Возможность изменить дистанцию рендера

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ int main()
 
     int menuIndex = 0;
 
-    vector<wstring>menuName =   {L"Start",  L"MapWidth: ",      L"MapHeight: ",     L"FPS: ",    L"ScreenWidth: ",    L"ScreenHeight: " , L"RenderDistance:", L"Minimap:"};
+    vector<wstring>menuName =   {L"Start",  L"MapWidth: ",      L"MapHeight: ",     L"FPS: ",    L"ScreenWidth: ",    L"ScreenHeight: " , L"RenderDistance:2", L"Minimap:"};
     vector<int*>menuVar =       {nullptr,   &mapWidth,          &mapHeight,         &FPS,        &screenWidth,        &screenHeight     , &renderDistance,
                                  nullptr};
     vector<int>increment =      {NULL,      2,                  2,                  30,          10,                  10                , 2, NULL};
